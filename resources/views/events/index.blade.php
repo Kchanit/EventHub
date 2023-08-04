d9cfeba33d8ba1292a236691786a5da9d8758e35
Kchanit <chanitaphon.l@ku.th>
2023-08-04 13:38:57 +0700

    update event card style

--- resources/views/events/index.blade.php
+++ resources/views/events/index.blade.php
@@ -5,7 +5,7 @@
     <div class="flex items-center justify-between">
         <div>
 
-            <h1 class="text-2xl">
+            <h1 class="text-3xl font-bold">
                 All Events
             </h1>
             <p class="text-sm text-gray-500">
@@ -77,149 +77,181 @@
     <div class="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
         <!-- Grid -->
         <div class="grid sm:grid-cols-2 lg:grid-cols-3 gap-6">
-            <!-- Card -->
-            <div class="group flex flex-col h-full bg-white border border-gray-200 shadow-sm rounded-xl dark:bg-slate-900 dark:border-gray-700 dark:shadow-slate-700/[.7]">
-                <div class="h-52 flex flex-col justify-center items-center bg-gray-300 rounded-t-xl">
 
-                </div>
-                <div class="p-4 md:p-6">
-                    <span class="block mb-1 text-xs font-semibold uppercase text-blue-600 dark:text-blue-500">
-                        20-21 July
-                    </span>
-                    <h3 class="text-xl font-semibold text-gray-800 dark:text-gray-300 dark:hover:text-white">
-                        Freshy Night
-                    </h3>
-                    <p class="mt-3 text-gray-500">
-                        Something or somewhere about this event.
-                    </p>
-                </div>
-                <div class="mt-auto flex border-t border-gray-200 divide-x divide-gray-200 dark:border-gray-700 dark:divide-gray-700">
-                    <a class="w-full py-3 px-4 inline-flex justify-center items-center gap-2 rounded-xl font-medium bg-white text-gray-700 shadow-sm align-middle hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-blue-600 transition-all text-sm sm:p-4 dark:bg-slate-900 dark:hover:bg-slate-800 dark:border-gray-700 dark:text-gray-400 dark:hover:text-white dark:focus:ring-offset-gray-800" href="#">
-                        Apply
-                    </a>
-                </div>
+            <!-- Card -->
+            <div class=" rounded-3xl overflow-hidden shadow-lg shadow-gray-300 dark:shadow-gray-900 bg-white dark:bg-gray-800 duration-150 hover:-translate-y-1">
+                <!-- Clickable Area -->
+                <a _href="#" class="cursor-pointer">
+                    <figure class="group relative">
+                        <!-- Image -->
+                        <img class="bg-gray-300  h-72 w-full object-cover z-0" />
+                        <div class="p-4">
+                            <!-- Date -->
+                            <span class="block mb-1 text-xs font-semibold uppercase text-blue-600 dark:text-blue-500">
+                                20-21 July
+                            </span>
+                            <!-- Title -->
+                            <p class="text-lg mb-1 font-bold leading-relaxed text-gray-800 dark:text-gray-300">
+                                Freshy Night
+                            </p>
+                            <p class="text-gray-500">
+                                Something or somewhere about this event.
+                            </p>
+                        </div>
+                    </figure>
+                </a>
             </div>
             <!-- End Card -->
 
-
-
             <!-- Card -->
-            <div class="group flex flex-col h-full bg-white border border-gray-200 shadow-sm rounded-xl dark:bg-slate-900 dark:border-gray-700 dark:shadow-slate-700/[.7]">
-                <div class="h-52 flex flex-col justify-center items-center bg-gray-300 rounded-t-xl">
-
-                </div>
-                <div class="p-4 md:p-6">
-                    <span class="block mb-1 text-xs font-semibold uppercase text-blue-600 dark:text-blue-500">
-                        20-21 July
-                    </span>
-                    <h3 class="text-xl font-semibold text-gray-800 dark:text-gray-300 dark:hover:text-white">
-                        Freshy Night
-                    </h3>
-                    <p class="mt-3 text-gray-500">
-                        Something or somewhere about this event.
-                    </p>
-                </div>
-                <div class="mt-auto flex border-t border-gray-200 divide-x divide-gray-200 dark:border-gray-700 dark:divide-gray-700">
-                    <a class="w-full py-3 px-4 inline-flex justify-center items-center gap-2 rounded-xl font-medium bg-white text-gray-700 shadow-sm align-middle hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-blue-600 transition-all text-sm sm:p-4 dark:bg-slate-900 dark:hover:bg-slate-800 dark:border-gray-700 dark:text-gray-400 dark:hover:text-white dark:focus:ring-offset-gray-800" href="#">
-                        Apply
-                    </a>
-                </div>
-            </div>
-            <!-- End Card -->
-            <!-- Card -->
-            <div class="group flex flex-col h-full bg-white border border-gray-200 shadow-sm rounded-xl dark:bg-slate-900 dark:border-gray-700 dark:shadow-slate-700/[.7]">
-                <div class="h-52 flex flex-col justify-center items-center bg-gray-300 rounded-t-xl">
-
-                </div>
-                <div class="p-4 md:p-6">
-                    <span class="block mb-1 text-xs font-semibold uppercase text-blue-600 dark:text-blue-500">
-                        20-21 July
-                    </span>
-                    <h3 class="text-xl font-semibold text-gray-800 dark:text-gray-300 dark:hover:text-white">
-                        Freshy Night
-                    </h3>
-                    <p class="mt-3 text-gray-500">
-                        Something or somewhere about this event.
-                    </p>
-                </div>
-                <div class="mt-auto flex border-t border-gray-200 divide-x divide-gray-200 dark:border-gray-700 dark:divide-gray-700">
-                    <a class="w-full py-3 px-4 inline-flex justify-center items-center gap-2 rounded-xl font-medium bg-white text-gray-700 shadow-sm align-middle hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-blue-600 transition-all text-sm sm:p-4 dark:bg-slate-900 dark:hover:bg-slate-800 dark:border-gray-700 dark:text-gray-400 dark:hover:text-white dark:focus:ring-offset-gray-800" href="#">
-                        Apply
-                    </a>
-                </div>
-            </div>
-            <!-- End Card -->
-            <!-- Card -->
-            <div class="group flex flex-col h-full bg-white border border-gray-200 shadow-sm rounded-xl dark:bg-slate-900 dark:border-gray-700 dark:shadow-slate-700/[.7]">
-                <div class="h-52 flex flex-col justify-center items-center bg-gray-300 rounded-t-xl">
-
-                </div>
-                <div class="p-4 md:p-6">
-                    <span class="block mb-1 text-xs font-semibold uppercase text-blue-600 dark:text-blue-500">
-                        20-21 July
-                    </span>
-                    <h3 class="text-xl font-semibold text-gray-800 dark:text-gray-300 dark:hover:text-white">
-                        Freshy Night
-                    </h3>
-                    <p class="mt-3 text-gray-500">
-                        Something or somewhere about this event.
-                    </p>
-                </div>
-                <div class="mt-auto flex border-t border-gray-200 divide-x divide-gray-200 dark:border-gray-700 dark:divide-gray-700">
-                    <a class="w-full py-3 px-4 inline-flex justify-center items-center gap-2 rounded-xl font-medium bg-white text-gray-700 shadow-sm align-middle hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-blue-600 transition-all text-sm sm:p-4 dark:bg-slate-900 dark:hover:bg-slate-800 dark:border-gray-700 dark:text-gray-400 dark:hover:text-white dark:focus:ring-offset-gray-800" href="#">
-                        Apply
-                    </a>
-                </div>
-            </div>
-            <!-- End Card -->
-            <!-- Card -->
-            <div class="group flex flex-col h-full bg-white border border-gray-200 shadow-sm rounded-xl dark:bg-slate-900 dark:border-gray-700 dark:shadow-slate-700/[.7]">
-                <div class="h-52 flex flex-col justify-center items-center bg-gray-300 rounded-t-xl">
-
-                </div>
-                <div class="p-4 md:p-6">
-                    <span class="block mb-1 text-xs font-semibold uppercase text-blue-600 dark:text-blue-500">
-                        20-21 July
-                    </span>
-                    <h3 class="text-xl font-semibold text-gray-800 dark:text-gray-300 dark:hover:text-white">
-                        Freshy Night
-                    </h3>
-                    <p class="mt-3 text-gray-500">
-                        Something or somewhere about this event.
-                    </p>
-                </div>
-                <div class="mt-auto flex border-t border-gray-200 divide-x divide-gray-200 dark:border-gray-700 dark:divide-gray-700">
-                    <a class="w-full py-3 px-4 inline-flex justify-center items-center gap-2 rounded-xl font-medium bg-white text-gray-700 shadow-sm align-middle hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-blue-600 transition-all text-sm sm:p-4 dark:bg-slate-900 dark:hover:bg-slate-800 dark:border-gray-700 dark:text-gray-400 dark:hover:text-white dark:focus:ring-offset-gray-800" href="#">
-                        Apply
-                    </a>
-                </div>
-            </div>
-            <!-- End Card -->
-            <!-- Card -->
-            <div class="group flex flex-col h-full bg-white border border-gray-200 shadow-sm rounded-xl dark:bg-slate-900 dark:border-gray-700 dark:shadow-slate-700/[.7]">
-                <div class="h-52 flex flex-col justify-center items-center bg-gray-300 rounded-t-xl">
-
-                </div>
-                <div class="p-4 md:p-6">
-                    <span class="block mb-1 text-xs font-semibold uppercase text-blue-600 dark:text-blue-500">
-                        20-21 July
-                    </span>
-                    <h3 class="text-xl font-semibold text-gray-800 dark:text-gray-300 dark:hover:text-white">
-                        Freshy Night
-                    </h3>
-                    <p class="mt-3 text-gray-500">
-                        Something or somewhere about this event.
-                    </p>
-                </div>
-                <div class="mt-auto flex border-t border-gray-200 divide-x divide-gray-200 dark:border-gray-700 dark:divide-gray-700">
-                    <a class="w-full py-3 px-4 inline-flex justify-center items-center gap-2 rounded-xl font-medium bg-white text-gray-700 shadow-sm align-middle hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-blue-600 transition-all text-sm sm:p-4 dark:bg-slate-900 dark:hover:bg-slate-800 dark:border-gray-700 dark:text-gray-400 dark:hover:text-white dark:focus:ring-offset-gray-800" href="#">
-                        Apply
-                    </a>
-                </div>
+            <div class=" rounded-3xl overflow-hidden shadow-lg shadow-gray-300 dark:shadow-gray-900 bg-white dark:bg-gray-800 duration-150 hover:-translate-y-1">
+                <!-- Clickable Area -->
+                <a _href="#" class="cursor-pointer">
+                    <figure class="group relative">
+                        <!-- Image -->
+                        <img class="bg-gray-300  h-72 w-full object-cover z-0" />
+                        <div class="p-4">
+                            <!-- Date -->
+                            <span class="block mb-1 text-xs font-semibold uppercase text-blue-600 dark:text-blue-500">
+                                20-21 July
+                            </span>
+                            <!-- Title -->
+                            <p class="text-lg mb-1 font-bold leading-relaxed text-gray-800 dark:text-gray-300">
+                                Freshy Night
+                            </p>
+                            <p class="text-gray-500">
+                                Something or somewhere about this event.
+                            </p>
+                        </div>
+                    </figure>
+                </a>
             </div>
             <!-- End Card -->
 
+            <!-- Card -->
+            <div class=" rounded-3xl overflow-hidden shadow-lg shadow-gray-300 dark:shadow-gray-900 bg-white dark:bg-gray-800 duration-150 hover:-translate-y-1">
+                <!-- Clickable Area -->
+                <a _href="#" class="cursor-pointer">
+                    <figure class="group relative">
+                        <!-- Image -->
+                        <img class="bg-gray-300  h-72 w-full object-cover z-0" />
+                        <div class="p-4">
+                            <!-- Date -->
+                            <span class="block mb-1 text-xs font-semibold uppercase text-blue-600 dark:text-blue-500">
+                                20-21 July
+                            </span>
+                            <!-- Title -->
+                            <p class="text-lg mb-1 font-bold leading-relaxed text-gray-800 dark:text-gray-300">
+                                Freshy Night
+                            </p>
+                            <p class="text-gray-500">
+                                Something or somewhere about this event.
+                            </p>
+                        </div>
+                    </figure>
+                </a>
+            </div>
+            <!-- End Card -->
 
+            <!-- Card -->
+            <div class=" rounded-3xl overflow-hidden shadow-lg shadow-gray-300 dark:shadow-gray-900 bg-white dark:bg-gray-800 duration-150 hover:-translate-y-1">
+                <!-- Clickable Area -->
+                <a _href="#" class="cursor-pointer">
+                    <figure class="group relative">
+                        <!-- Image -->
+                        <img class="bg-gray-300  h-72 w-full object-cover z-0" />
+                        <div class="p-4">
+                            <!-- Date -->
+                            <span class="block mb-1 text-xs font-semibold uppercase text-blue-600 dark:text-blue-500">
+                                20-21 July
+                            </span>
+                            <!-- Title -->
+                            <p class="text-lg mb-1 font-bold leading-relaxed text-gray-800 dark:text-gray-300">
+                                Freshy Night
+                            </p>
+                            <p class="text-gray-500">
+                                Something or somewhere about this event.
+                            </p>
+                        </div>
+                    </figure>
+                </a>
+            </div>
+            <!-- End Card -->
+
+            <!-- Card -->
+            <div class=" rounded-3xl overflow-hidden shadow-lg shadow-gray-300 dark:shadow-gray-900 bg-white dark:bg-gray-800 duration-150 hover:-translate-y-1">
+                <!-- Clickable Area -->
+                <a _href="#" class="cursor-pointer">
+                    <figure class="group relative">
+                        <!-- Image -->
+                        <img class="bg-gray-300  h-72 w-full object-cover z-0" />
+                        <div class="p-4">
+                            <!-- Date -->
+                            <span class="block mb-1 text-xs font-semibold uppercase text-blue-600 dark:text-blue-500">
+                                20-21 July
+                            </span>
+                            <!-- Title -->
+                            <p class="text-lg mb-1 font-bold leading-relaxed text-gray-800 dark:text-gray-300">
+                                Freshy Night
+                            </p>
+                            <p class="text-gray-500">
+                                Something or somewhere about this event.
+                            </p>
+                        </div>
+                    </figure>
+                </a>
+            </div>
+            <!-- End Card -->
+
+            <!-- Card -->
+            <div class=" rounded-3xl overflow-hidden shadow-lg shadow-gray-300 dark:shadow-gray-900 bg-white dark:bg-gray-800 duration-150 hover:-translate-y-1">
+                <!-- Clickable Area -->
+                <a _href="#" class="cursor-pointer">
+                    <figure class="group relative">
+                        <!-- Image -->
+                        <img class="bg-gray-300  h-72 w-full object-cover z-0" />
+                        <div class="p-4">
+                            <!-- Date -->
+                            <span class="block mb-1 text-xs font-semibold uppercase text-blue-600 dark:text-blue-500">
+                                20-21 July
+                            </span>
+                            <!-- Title -->
+                            <p class="text-lg mb-1 font-bold leading-relaxed text-gray-800 dark:text-gray-300">
+                                Freshy Night
+                            </p>
+                            <p class="text-gray-500">
+                                Something or somewhere about this event.
+                            </p>
+                        </div>
+                    </figure>
+                </a>
+            </div>
+            <!-- End Card -->
+
+            <!-- Card -->
+            <div class=" rounded-3xl overflow-hidden shadow-lg shadow-gray-300 dark:shadow-gray-900 bg-white dark:bg-gray-800 duration-150 hover:-translate-y-1">
+                <!-- Clickable Area -->
+                <a _href="#" class="cursor-pointer">
+                    <figure class="group relative">
+                        <!-- Image -->
+                        <img class="bg-gray-300  h-72 w-full object-cover z-0" />
+                        <div class="p-4">
+                            <!-- Date -->
+                            <span class="block mb-1 text-xs font-semibold uppercase text-blue-600 dark:text-blue-500">
+                                20-21 July
+                            </span>
+                            <!-- Title -->
+                            <p class="text-lg mb-1 font-bold leading-relaxed text-gray-800 dark:text-gray-300">
+                                Freshy Night
+                            </p>
+                            <p class="text-gray-500">
+                                Something or somewhere about this event.
+                            </p>
+                        </div>
+                    </figure>
+                </a>
+            </div>
+            <!-- End Card -->
 
         </div>
         <!-- End Grid -->
