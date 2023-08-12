5a202fa2ec71cc0d7a7b287174fb6fc9d03d1977
Kchanit <chanitaphon.l@ku.th>
2023-08-12 16:10:05 +0700

    feat: fixed tabs bug

--- resources/views/events/show.blade.php
+++ resources/views/events/show.blade.php
@@ -1,7 +1,9 @@
 @extends('layouts.main')
 
 @section('content')
-    @include('layouts.subviews.tabs')
+    @can('update', $event)
+        @include('layouts.subviews.tabs')
+    @endcan
     <section class="bg-white">
         <div class="py-8 px-4 mx-auto max-w-2xl ">
             <h2 class="mb-2 text-xl font-semibold leading-none text-gray-900 md:text-2xl dark:text-white">{{ $event->title }}
